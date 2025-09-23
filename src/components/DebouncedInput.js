abc4a8edfeb712d629d80e0d3e17ff208e397b24
rock7777 <61678762+Rock7777@users.noreply.github.com>
2025-09-23 11:42:43 +0000

    debouncing_input changes

--- src/components/DebouncedInput.js
+++ src/components/DebouncedInput.js
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 const DebouncedInput = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [debouncedValue, setDebouncedValue] = useState("");
+  const debounce = (f1, delay) => {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      setTimeout(() => {
+        f1(...args);
+      }, delay);
+    };
+  };
+  const handleChange = (e) => {
+    const userInput = e.target.value;
+    setInputValue(userInput);
+    handleInput(userInput);
+  };
+  const handleInput = debounce((value) => {
+    setDebouncedValue(value);
+  }, 2000);
+  const submitHandler = () => {
+    alert(`${debouncedValue}`);
+    setInputValue("");
+  };
   return (
     <div>
       <h1>DebouncedInput</h1>
+      <input type="text" value={inputValue} onChange={(e) => handleChange(e)} />
+      <button onClick={submitHandler}>Submit</button>
     </div>
   );
 };
